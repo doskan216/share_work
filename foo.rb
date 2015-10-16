0cf2f06c488ead4f1f4455cdbb1a431fdb9061d6
K M <mk@test.com>
2015-10-16 15:38:30 +0900

    ruby 追加

---
+++ foo.rb
@@ -0,0 +1,16 @@
+require 'byebug'
+
+class Foo
+  def foo
+    return "bar"
+  end
+
+  def hello_world(name)
+    return "Hello #{name}."
+  end
+end
+
+f = Foo.new
+byebug
+f.foo
+f.hello_world("Ruby")
